93ee3a7963ef72b666dfadd5c79a027abe271d68
Amanuel Shiferaw <123aman64@gmail.com>
2022-07-02 17:10:37 +0300

    Add movies Component

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-
-  constructor() { }
+  movies: Movie[] = [];
+  @Input() title: string = '';
+  constructor(private movieService: MoviesService) {}
 
   ngOnInit(): void {
+    this.movieService
+      .getMovies('popular', 20)
+      .subscribe((movies) => (this.movies = movies));
   }
-
 }
