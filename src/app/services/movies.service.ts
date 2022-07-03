89a815d769282fff57ac23eb093a4f0ca129274c
Amanuel Shiferaw <123aman64@gmail.com>
2022-07-03 19:26:36 +0300

    Add Search Movies  method added

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -23,6 +23,15 @@ export class MoviesService {
         })
       );
   }
+  searchMovies() {
+    return this.http
+      .get<MovieDto>(`${this.baseUrl}/movie/popular?api_key=${this.apiKey}`)
+      .pipe(
+        switchMap((res) => {
+          return of(res.results);
+        })
+      );
+  }
 
   getTvs(type: string = 'latest', count: number = 12) {
     return this.http
