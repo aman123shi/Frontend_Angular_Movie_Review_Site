abc8585be21a5efb812cff9ecee51fbbf702a647
Amanuel Shiferaw <123aman64@gmail.com>
2022-07-03 22:18:02 +0300

    Fix Slider to Handle Also Single Movie as a Banner

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,10 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { IMAGES_SIZES } from '../../constants/images-sizes';
 import { Movie } from '../../models/movie';
@@ -7,18 +13,25 @@ import { Movie } from '../../models/movie';
   selector: 'slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
-  animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])]
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
-
+  @Input() isBanner: boolean = false;
   currentSlideIndex: number = 0;
 
   readonly imagesSizes = IMAGES_SIZES;
 
   ngOnInit(): void {
-    setInterval(() => {
-      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
-    }, 5000);
+    if (!this.isBanner) {
+      setInterval(() => {
+        this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+      }, 5000);
+    }
   }
 }
