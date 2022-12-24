2bee96bd91c182db96541611c4436decbf90ed82
Amanuel Shiferaw <123aman64@gmail.com>
2022-12-24 13:02:22 +0300

    Fix build outPut Path

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const path =require("path");
+const path = require("path");
 const app = express();
-app.use(express.static('./dist/frontend'));
+app.use(express.static("./dist"));
 
-app.get('/*',(req,res)=>{
-    res.sendFile('index.html',{root:'dist/frontend/'});
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: "dist" });
 });
 
 app.listen(process.env.PORT || 8080);
